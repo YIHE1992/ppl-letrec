29a0369b216940ebb1cac5a3b7e9dc49262fe1c4
sky91 <feitiandaxia1991@163.com>
2017-11-13 00:47:43 +0800

    if isZero

---
+++ src/main/java/yi/letlangproj/exp/LetRecExpression.java
@@ -0,0 +1,14 @@
+package yi.letlangproj.exp;
+
+import yi.letlangproj.Environment;
+import yi.letlangproj.Expression;
+
+/**
+ * @author sky91 - feitiandaxia1991@163.com
+ */
+public class LetRecExpression implements Expression {
+    @Override
+    public int evaluate(Environment environment) {
+        return 0;
+    }
+}
---
+++ src/main/java/yi/letlangproj/exp/LetRecExpressionParser.java
@@ -0,0 +1,17 @@
+package yi.letlangproj.exp;
+
+import yi.letlangproj.ExpressionParser;
+import yi.letlangproj.ParseException;
+import yi.letlangproj.Token;
+
+import java.util.List;
+
+/**
+ * @author sky91 - feitiandaxia1991@163.com
+ */
+public class LetRecExpressionParser implements ExpressionParser {
+    @Override
+    public ParseResult parse(List<Token> tokenList) throws ParseException {
+        return null;
+    }
+}
